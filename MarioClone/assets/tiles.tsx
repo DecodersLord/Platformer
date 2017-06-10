6002f7c97e072a285ca10df1bb9d7fd922664e20
DecodersLord <priyank.sevak1996@gmail.com>
2017-06-11 01:57:35 +0530

    Initial commit

---
+++ MarioClone/assets/tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tiles" tilewidth="35" tileheight="35" spacing="1" tilecount="4" columns="2">
+ <image source="Images/greenground.png" width="82" height="82"/>
+</tileset>
